95ca794db3bbcd76e6e6101f45fe8dce4aed119c
Gustavo Zonta <gustavozontauk@gmail.com>
2023-03-28 12:38:34 +0100

    :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+<h1 align="center"> Fortune Cookie </h1>
+
+<p align="center">
+  <a href="#-tecnologias">Technology</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">License</a>
+</p>
+
+<br>
+
+<h2 align="center"> Design </h2>
+
+<p align="center">
+  <img alt="fortunecookiefirstpage" src="./assets/fortunecookiefirst" width="100%">
+  </br>
+  <img alt="fortunecookiesecondpage" src="./assets/fortunecookiesecond" width="100%">
+</p>
+
+## 🚀 Technology
+
+This project was developed with the technologies below:
+
+- HTML and CSS
+- Git and Github
+- JavaScript
+
+## 💻 Project
+
+<p align="start">
+This is a Fortune Cookie Game. It is a simple game to discover any random luck message, only press enter or click on the cookie and you will find out </p>
+
+### 💻  Project Themes
+
+- HTML data structure
+- Animation with CSS
+- Functions in JavaScript
+- Manipulation of DOM
+- JS Math Library()
+- Callback Functions
+- Arrays
+
+
+## :memo: License
+
+This project is above the MIT license.
+
+## link
+
+<a href="https://gustavozonta.github.io/fortunecookiegame/" target="_blank"> Fortune Cookie </a>
+
+---
+<p align="center"> Made  ♥  by Gustavo Zonta</p>
---
+++ main.js
@@ -0,0 +1,54 @@
+//variables
+const screen1 = document.querySelector('.screen1');
+const screen2 = document.querySelector('.screen2');
+const cookieOpen = document.querySelector('#cookieOpen'); 
+const anotherCookie = document.querySelector('#anotherCookie');
+let fortuneRandomPhrase = document.querySelector('.fortuneRandomPhrase p');
+
+
+//logic
+let fortunePhrases = [
+  "Delight the world with compassion, kindness and grace",
+  "The early bird gets the worm, but the second mouse gets the cheese.",
+  "Some days you are pigeon, some days you are statue. Today, bring umbrella.",
+  "The fortune you seek is in another cookie.",
+  "Be on the alert to recognize your prime at whatever time of your life it may occur.",
+  "Your reality check about to bounce.",
+  "Tension is who you think you should be. Relaxation is who you are.",
+  "When blind leading the blind……..get out of the way.",
+  "Everyone seems normal until you get to know them.",
+  "Only difference between rut and a grave is depth.",
+];
+
+
+//events
+cookieOpen.addEventListener('click', phraseAppear)
+anotherCookie.addEventListener('click', anotherCookieClick);
+document.addEventListener('keydown', enterButton)
+document.addEventListener('keyup', leaveButton)
+
+//functions
+function phraseAppear(event) {
+  event.preventDefault(event);
+
+  let randomPhrases = Math.floor(Math.random() * fortunePhrases.length)
+  fortuneRandomPhrase.innerHTML = fortunePhrases[randomPhrases];
+
+  toggleClick();
+}
+
+function anotherCookieClick(event) {
+  event.preventDefault(event);
+  toggleClick();
+}
+
+function toggleClick() {
+  screen1.classList.toggle('hide');
+  screen2.classList.toggle('hide');
+}
+
+function enterButton(e) {
+    if(e.key == 'Enter' && screen2.classList.contains('hide')) {
+      phraseAppear(e);
+    }
+  }
